569cb636b441bfcc3c0753fdc8533fec94898bde
Haythem <sinnner6@gmail.com>
2023-01-08 18:31:34 +0100

    added products component with basic delete button

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  products: Array<any> | undefined;
 
+  constructor() {}
+
+  ngOnInit(): void {
+    this.products = [
+      { id: 1, name: 'Computer', price: 3000 },
+      { id: 2, name: 'Mobile', price: 2000 },
+      { id: 3, name: 'Laptop', price: 4000 },
+      { id: 4, name: 'Tablet', price: 1000 },
+      { id: 5, name: 'Camera', price: 5000 },
+    ];
+  }
+
+  handleDeleteProduct(product: any) {
+    const index = this.products?.indexOf(product);
+    this.products?.splice(index!, 1);
+  }
 }
